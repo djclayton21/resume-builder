0834582af957d74ddba224d4143d63a3328503fb
djclayton21 <clayton.daniel.j@gmail.com>
2020-01-18 13:18:41 -0700

    switched to FileReader api

--- generator/parser.js
+++ generator/parser.js
@@ -1,14 +1,24 @@
-const showdown = window.showdown;
-const mdConverter = new showdown.Converter();
-
 const resume = document.querySelector('#resume-container');
 const markdownInput = document.querySelector('#markdown-input');
 const markdownForm = document.querySelector('#markdown-form');
 
-markdownForm.addEventListener('submit', async event => {
+const showdown = new window.showdown.Converter();
+const reader = new FileReader();
+reader.onload = event => {
+  const mdText = event.target.result;
+  const mdHtml = showdown.makeHtml(mdText);
+  //do we need to clean?
+  resume.innerHTML = mdHtml;
+};
+reader.onerror = () => {
+  console.error('File could not be loaded');
+};
+
+markdownForm.addEventListener('submit', event => {
   event.preventDefault();
-  const markdownFile = markdownInput.files[0];
-  const text = await markdownFile.text();
-  const html = mdConverter.makeHtml(text);
-  resume.innerHTML = html;
+  const file = markdownInput.files[0];
+  reader.readAsText(file);
 });
+//read file function
+//convert file function
+//transform file function
