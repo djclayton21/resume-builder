8a667a8cca0523e3d7ed51e24033bc5071c3e400
djclayton21 <clayton.daniel.j@gmail.com>
2020-02-05 14:53:45 -0700

    styled resume basic

--- generator/app.js
+++ generator/app.js
@@ -1,7 +1,3 @@
-//setup
-const showdown = new window.showdown.Converter();
-const mdReader = new FileReader();
-
 //grab stuff
 const uploadForm = document.querySelector('#upload-form');
 const mdTextArea = document.querySelector('#markdown-textarea');
@@ -10,6 +6,8 @@ const fileName = document.querySelector('#fileName');
 const printButton = document.querySelector('#print-button');
 
 //file reader
+const showdown = new window.showdown.Converter();
+const mdReader = new FileReader();
 mdReader.onload = event => {
   const mdText = event.target.result;
   //TODO: validate markdown
@@ -18,9 +16,11 @@ mdReader.onload = event => {
   const resume = convert(html); //create dom nodes and wrap h2s in sections
   display(resume);
 };
+
 mdReader.onerror = () => {
   console.error('File could not be loaded');
 };
+
 //upload markdown
 uploadForm.addEventListener('submit', event => {
   event.preventDefault();
@@ -37,32 +37,50 @@ mdTextArea.addEventListener('input', event => {
   const resume = convert(html); //create dom nodes and wrap h2s in sections
   display(resume);
 });
+
 //convert text to html and transform
 function convert(html) {
   const resume = { sections: [], fileName: '' };
+
   const temp = document.createElement('template');
   temp.innerHTML = html;
   const elements = [...temp.content.children];
+
   elements.forEach(element => {
     if (element.tagName === 'H1') {
+      //
       resume.fileName = element.textContent.split(' ').join('_');
     } else if (element.tagName === 'H2') {
+      //create a new section for h2s
       const newSection = document.createElement('section');
       newSection.id = `section${resume.sections.length + 1}`;
       newSection.appendChild(element);
       resume.sections.push(newSection);
+    } else if (
+      element.tagName === 'P' &&
+      element.textContent === '\\pagebreak'
+    ) {
+      //TODO: pagebreaks
     } else if (resume.sections.length) {
+      //for other elements, append to last section
       resume.sections[resume.sections.length - 1].appendChild(element);
     }
   });
   return resume;
 }
+
 //display resume to preview
 function display(resume) {
   fileName.textContent = resume.fileName;
   document.title = resume.fileName;
   preview.innerHTML = '';
-  resume.sections.forEach(section => preview.appendChild(section));
+
+  const page1 = document.createElement('div');
+  page1.classList = 'page page1';
+  preview.appendChild(page1);
+  let currentPage = page1;
+  //TODO: pages/ columns
+  resume.sections.forEach(section => currentPage.appendChild(section));
 }
 
 //print current version
