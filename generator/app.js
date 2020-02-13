bcea943ef4ff503a5f8aafa15823c2a84642413c
djclayton21 <clayton.daniel.j@gmail.com>
2020-02-12 18:35:12 -0700

    local storage save/retreive

--- generator/app.js
+++ generator/app.js
@@ -1,41 +1,51 @@
 //grab stuff
-const uploadForm = document.querySelector('#upload-form');
-const mdTextArea = document.querySelector('#markdown-textarea');
+const showdown = new window.showdown.Converter();
 const previewContainer = document.querySelector('#preview');
 const fileName = document.querySelector('#fileName');
-
-//file reader
-const showdown = new window.showdown.Converter();
+//setup file reader
 const mdReader = new FileReader();
 mdReader.onload = event => {
   const mdText = event.target.result;
   //TODO: validate markdown
-  mdTextArea.value = mdText;
+  saveToLocal(mdText);
+  mdInputArea.value = mdText;
+
   const html = showdown.makeHtml(mdText);
-  const resume = convert(html); //create dom nodes and wrap h2s in sections
+  const resume = convert(html);
   preview(resume);
 };
-
 mdReader.onerror = () => {
   console.error('File could not be loaded');
 };
 
-//upload markdown
-uploadForm.addEventListener('submit', event => {
-  event.preventDefault();
-  const upload = event.target.querySelector('#upload-input').files[0];
-  //TODO: validate
-  mdReader.readAsText(upload);
+//upload and read markdown file
+const upload = document.querySelector('#file-upload');
+upload.addEventListener('change', event => {
+  const file = event.target.files[0];
+  mdReader.readAsText(file);
 });
-
-//live preview
-mdTextArea.addEventListener('input', event => {
+//handle changes to text area
+const mdInputArea = document.querySelector('#markdown-textarea');
+mdInputArea.addEventListener('input', event => {
   const mdText = event.target.value;
   //TODO: validate and clean input. #yolo
+  saveToLocal(mdText);
   const html = showdown.makeHtml(mdText);
   const resume = convert(html); //create dom nodes and wrap h2s in sections
   preview(resume);
 });
+
+//local storage
+function saveToLocal(mdText) {
+  localStorage.setItem('mdResume', mdText);
+}
+function getFromLocal() {
+  const mdText = localStorage.getItem('mdResume');
+  mdInputArea.value = mdText;
+  const html = showdown.makeHtml(mdText);
+  const resume = convert(html);
+  preview(resume)
+}
 
 //convert text to html and transform
 function convert(html) {
@@ -69,21 +79,19 @@ function preview(resume) {
   previewContainer.innerHTML = '';
   resume.sections.forEach(section => previewContainer.appendChild(section));
 }
-
 //change resume style
-const resumeStyles = document.querySelectorAll('.resume-style');
+const resumeStylesheets = document.querySelectorAll('.resume-style');
 const styleForm = document.querySelector('#style-form');
 styleForm.addEventListener('change', selectStyle);
 
-resumeStyles.forEach(style => {
+resumeStylesheets.forEach(style => {
   if (style.title !== styleForm.style.value) {
     style.disabled = true;
   }
 });
-
 function selectStyle() {
   const newStyle = this.style.value;
-  resumeStyles.forEach(style => {
+  resumeStylesheets.forEach(style => {
     if (style.title === newStyle) {
       style.disabled = false;
     } else {
@@ -92,13 +100,15 @@ function selectStyle() {
   });
   updateFileName();
 }
-
+//add current style to the end of filename
 function updateFileName() {
   const oldFileName = fileName.textContent;
   const words = oldFileName.split('_');
-  //get style names
-  const styleNames = [];
-  resumeStyles.forEach(style => styleNames.push(style.title));
+
+  //get style names from sheets
+  const styleNames = []; //get
+  resumeStylesheets.forEach(style => styleNames.push(style.title));
+
   if (styleNames.includes(words[words.length - 1])) {
     words.pop();
     words.push(styleForm.style.value);
@@ -113,3 +123,5 @@ const printButton = document.querySelector('#print-button');
 printButton.addEventListener('click', () => {
   window.print();
 });
+
+getFromLocal();
